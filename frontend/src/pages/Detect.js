c5ada8486ec80a13dd50ca2c5acb3189cbeac773
Yakshith Kommineni <Yakshith.kommineni@gmail.com>
2025-02-14 22:30:29 -0500

    api issue

--- frontend/src/pages/Detect.js
+++ frontend/src/pages/Detect.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import "../App.css";
 import { Button } from "../components/Button";
 import ReportModal from "../components/reportModal";
-const FLASK_API_BASE_URL = process.env.BASE_URL ||'http://localhost:5000' //'https://cleartrust-mvp-1.onrender.com:10000'
+const FLASK_API_BASE_URL = 'https://cleartrust-mvp.onrender.com' //process.env.BASE_URL ||'http://localhost:5000' //'https://cleartrust-mvp-1.onrender.com:10000'
 
 function Detect() {
   const [activeTab, setActiveTab] = useState("sms"); // To toggle between tabs
